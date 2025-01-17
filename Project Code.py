5c7281b897a5bb1b5383933a72d0e26f9afa60b6
Isabel Zemke <dzemke@cps.edu>
2025-01-17 10:33:49 -0600

    FINAL DRAFT

--- Project Code.py
+++ Project Code.py
@@ -10,7 +10,7 @@ data['MaritalStatus'].replace(['?','Married-civ-spouse', 'Divorced', 'Never-marr
 data['Race'].replace(['?','White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'],[-1,0,1,2,3,4],inplace=True)
 data['Gender'].replace(['?','Male','Female'],[-1,0,1],inplace=True)
 data['Money'].replace(['?','>50K', '<=50K'],[-1,0,1],inplace=True)
-x=data[['Age','Sector','EdNum','MaritalStatus','Race','Gender','Assists']].values
+x=data[['Age','Sector','EdNum','MaritalStatus','Race','Gender','Hrs']].values
 y=data["Money"].values
 print(x)
 print(y)
@@ -21,30 +21,30 @@ test['MaritalStatus'].replace(['?','Married-civ-spouse', 'Divorced', 'Never-marr
 test['Race'].replace(['?','White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'],[-1,0,1,2,3,4],inplace=True)
 test['Gender'].replace(['?','Male','Female'],[-1,0,1],inplace=True)
 test['Money'].replace(['?','>50K', '<=50K'],[-1,0,1],inplace=True)
-x_test = test[['Age','Sector','EdNum','MaritalStatus','Race','Gender','Assists']].values
+x_test = test[['Age','Sector','EdNum','MaritalStatus','Race','Gender','Hrs']].values
 y_test = test["Money"].values
 scaler = StandardScaler().fit(x)
 x = scaler.transform(x)
 model = linear_model.LogisticRegression().fit(x, y)
 print(f"Model Accuracy: {model.score(x, y)}")
-print("Testing Data:")
-x_test = scaler.transform(x_test)
-for index in range(len(x_test)):
-    x_line = x_test[index]
-    x_line = x_line.reshape(-1, 7)
-    # print(x_line)
-    y_pred = int(model.predict(x_line))
-    if y_pred == 0:
-        y_pred = ">50K"
+# print("Testing Data:")
+# x_test = scaler.transform(x_test)
+# for index in range(len(x_test)):
+#     x_line = x_test[index]
+#     x_line = x_line.reshape(-1, 7)
+#     # print(x_line)
+#     y_pred = int(model.predict(x_line))
+#     if y_pred == 0:
+#         y_pred = ">50K"
         
-    elif y_pred == 1:
-        y_pred = "<=50K"
+#     elif y_pred == 1:
+#         y_pred = "<=50K"
     
-    actual = y_test[index]
-    if actual == 0:
-        actual = ">50K"
-    elif actual == 1:
-        actual = "<=50K"
+#     actual = y_test[index]
+#     if actual == 0:
+#         actual = ">50K"
+#     elif actual == 1:
+#         actual = "<=50K"
         
-    print(f"Predicted Salary Range: {y_pred} Actual Salary Range: {actual}")
-    print("")
+#     print(f"Predicted Salary Range: {y_pred} Actual Salary Range: {actual}")
+#     print("")
